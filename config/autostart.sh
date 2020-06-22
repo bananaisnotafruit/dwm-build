cc70af52f2e1ecdfdaff1412f7acd187815d8369
adarsh <bananaisnotafruit69@gmail.com>
2020-06-22 18:51:53 +0530

    fixed bar

--- config/autostart.sh
+++ config/autostart.sh
@@ -5,8 +5,8 @@ nitrogen --restore &
 xset r rate 300 50
 xinput set-prop 12 298 -1
 while true; do
-    datetime="$(date +"%R %a %d %b")"
+    datetime="$(date +"%a %d %b %R")"
     vol="$(pulsemixer --get-volume | awk '{print $1}')"
-    xsetroot -name "${datetime} ${vol}"
+    xsetroot -name "${datetime} Vol:${vol}"
     sleep 1
 done &
