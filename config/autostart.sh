d23f2977d65e3deeddb880ae107b9b21dc8e13e7
adarsh <bananaisnotafruit69@gmail.com>
2020-07-03 12:30:31 +0530

    changed defaults

--- config/autostart.sh
+++ config/autostart.sh
@@ -4,6 +4,9 @@ nitrogen --restore &
 
 xset r rate 300 50
 xinput set-prop 12 298 -1
+
+export PATH="$HOME/scripts:$PATH"
+
 while true; do
     datetime="$(date +"%a %d %b %R")"
     vol="$(pulsemixer --get-volume | awk '{print $1}')"
