e6938b2eae19f128a645bc43667b1dba503cf2fb
adarsh <bananaisnotafruit69@gmail.com>
2020-07-22 10:40:16 +0530

    Changed theme

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 5;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -14,10 +14,15 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_orange[]      = "#F29F05";
 static const char col_blue[]        = "#1455D9";
+
+static const char col_bg[]  = "#5C6B72";
+static const char col_brd[] = "#EBE7E4";
+static const char col_bar[] = "#EBE7E4";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2   },
-	[SchemeSel]  = { col_gray4, col_blue,  col_orange  },
+	[SchemeSel]  = { col_brd, col_bg,    col_bar  },
 };
 
 /* tagging */
@@ -60,11 +65,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-
+/*  volume control */
 static const char *upvol[]   = { "pulsemixer", "--change-volume", "+5",     NULL };
 static const char *downvol[] = { "pulsemixer", "--change-volume", "-5",     NULL };
 static const char *mutevol[] = { "pulsemixer", "--toggle-mute", NULL };
-
+/*  custom scripts */
 static const char *dmenuascii[] = { "bash", ".config/dwm/ascii/main.sh", NULL };
 static const char *screenshot[] = { "screengrab", NULL };
 static const char *emacs[] = { "emacs", NULL };
