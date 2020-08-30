2a9ddb65830493c0e393fee76b05217c41438025
adarsh <bananaisnotafruit69@gmail.com>
2020-08-30 22:13:46 +0530

    using Awesome-Fonts in the dwm status

--- config/autostart.sh
+++ config/autostart.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# nitrogen --restore &
-feh -bg-scale .desktop.jpg
+nitrogen --restore &
+xcompmgr &
+budgie-polkit-dialog &
 
 xset r rate 300 50
 xinput set-prop 12 298 -1
+setxkbmap -option caps:escape
 
 export PATH="$HOME/scripts:$PATH"
 
 while true; do
-    datetime="$(date +"%a %d %b %R")"
+    datetime="$(date +" %a %d %b |  %R")"
     vol="$(pulsemixer --get-volume | awk '{print $1}')"
-    xsetroot -name "${datetime} Vol:${vol}"
+    xsetroot -name "${datetime} |  ${vol}"
     sleep 1
 done &
